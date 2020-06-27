940fb806e7b56884f28ac42354d45836480b140e
ediconsystems <milan.svec@ediconsystems.cz>
2020-06-27 15:54:35 +0200

    update 20200627

--- EdiconPdf/demo.cmd
+++ EdiconPdf/demo.cmd
@@ -1,15 +1,15 @@
 @echo Vytvoreni dokladu Edicon
 @pause
-EdiconPdf.exe -c 00030000071908_orig.pdf 00030000071908_orig.isdoc 00030000071908_new.pdf
+EdiconPdf.exe -c 0120010001_orig.pdf 0120010001_orig.isdoc 0120010001.pdf
 
 @echo.
-@echo ziskani dat z dokladu Edicon s ulozenim do souboru
+@echo Ziskani dat z dokladu Edicon s ulozenim do souboru
 @pause
-EdiconPdf.exe -x 00030000071908_new.pdf 00030000071908_extracted.isdoc
+EdiconPdf.exe -x 0120010001.pdf 0120010001_extracted.isdoc
 
 @echo.
 @echo Vypis dat z dokladu Edicon na obrazovku
 @pause
-EdiconPdf.exe -x 00030000071908_new.pdf
+EdiconPdf.exe -x 0120010001.pdf
 
 @pause
